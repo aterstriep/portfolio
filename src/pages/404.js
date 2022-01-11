e270ee148a250f4035586b21e9a05d3dfcd19a08
Ashley Terstriep <aterstriep@vervocity.io>
2022-01-10 22:14:39 -0600

    Fixed component import typo

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Helmet from "react-helmet"
 import Button from "../components/Button"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Container from "../components/Container"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
